171782cecdaa01a6a0b49d3033d40f389346e52a
Davide Ferro <davide.ferro@gmail.com>
2025-03-27 23:18:10 +0100

    fix LangFilesOrdering

--- lang/it/aiprovider_bedrock.php
+++ lang/it/aiprovider_bedrock.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['accesskeyid'] = 'ID chiave di accesso AWS';
+$string['accesskeyid_desc'] = 'L\'ID chiave di accesso AWS utilizzato per autenticarsi con Amazon Bedrock.';
 $string['action:generate_image:model'] = 'Modello IA';
 $string['action:generate_image:model_desc'] = 'Il modello utilizzato per generare immagini dai prompt degli utenti.';
 $string['action:generate_text:model'] = 'Modello IA';
@@ -32,12 +34,6 @@ $string['action:summarise_text:model'] = 'Modello IA';
 $string['action:summarise_text:model_desc'] = 'Il modello utilizzato per riassumere il testo fornito.';
 $string['action:summarise_text:systeminstruction'] = 'Istruzione di sistema';
 $string['action:summarise_text:systeminstruction_desc'] = 'Questa istruzione viene inviata al modello IA insieme al prompt dell\'utente. Si sconsiglia di modificare questa istruzione a meno che non sia assolutamente necessario.';
-$string['accesskeyid'] = 'ID chiave di accesso AWS';
-$string['accesskeyid_desc'] = 'L\'ID chiave di accesso AWS utilizzato per autenticarsi con Amazon Bedrock.';
-$string['secretaccesskey'] = 'Chiave di accesso segreta AWS';
-$string['secretaccesskey_desc'] = 'La chiave di accesso segreta AWS utilizzata per autenticarsi con Amazon Bedrock.';
-$string['region'] = 'Regione AWS';
-$string['region_desc'] = 'La regione AWS dove il servizio Amazon Bedrock è disponibile (es., us-east-1, us-west-2).';
 $string['enableglobalratelimit'] = 'Imposta limite di velocità a livello di sito';
 $string['enableglobalratelimit_desc'] = 'Limita il numero di richieste che il provider API Amazon Bedrock può ricevere in tutto il sito ogni ora.';
 $string['enableuserratelimit'] = 'Imposta limite di velocità utente';
@@ -51,5 +47,9 @@ $string['privacy:metadata:aiprovider_bedrock:model'] = 'Il modello utilizzato pe
 $string['privacy:metadata:aiprovider_bedrock:numberimages'] = 'Durante la generazione delle immagini, il numero di immagini utilizzate nella risposta.';
 $string['privacy:metadata:aiprovider_bedrock:prompttext'] = 'Il prompt di testo inserito dall\'utente utilizzato per generare la risposta.';
 $string['privacy:metadata:aiprovider_bedrock:responseformat'] = 'Durante la generazione delle immagini, il formato della risposta.';
+$string['region'] = 'Regione AWS';
+$string['region_desc'] = 'La regione AWS dove il servizio Amazon Bedrock è disponibile (es., us-east-1, us-west-2).';
+$string['secretaccesskey'] = 'Chiave di accesso segreta AWS';
+$string['secretaccesskey_desc'] = 'La chiave di accesso segreta AWS utilizzata per autenticarsi con Amazon Bedrock.';
 $string['userratelimit'] = 'Numero massimo di richieste per utente';
 $string['userratelimit_desc'] = 'Il numero di richieste consentite all\'ora, per utente.';
